3ef6b1c53ba846532c47344168c32a3a6414b446
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-17 10:32:53 +0000

    Added: Components

--- index.php
+++ index.php
@@ -8,7 +8,17 @@
     <link rel="stylesheet" href="/src/styles/main.css">
 </head>
 <body>
-    
+    <header class='flex items-center justify-between px-20'>
+        <p class="text-3xl">E-Learning</p>
+        <nav class='flex text-xl'>
+            <a class='Button' href="#">Home</a>
+            <a class='Button' href="#">Course</a>
+            <a class='Button' href="#">Course</a>
+            <a class='Button' href="#">Course</a>
+            <a class='Button' href="#">Login</a>
+        </nav>
+    </header>
+
     <script src="src/js/jquery-3.7.1.js"></script>
 </body>
 </html>
