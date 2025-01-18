6ac931f568370b33534d694ac18b47775479a406
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-18 11:28:40 +0000

    Added: Component PopularCourses.php

---
+++ src/components/PopularCourses.php
@@ -0,0 +1,41 @@
+<head>
+    <link rel="stylesheet" href="src/styles/main.css">
+</head>
+
+<div class='PopularCourses flex flex-wrap justify-center p-10'>
+    <div class='PCourses-Div'>
+        <img src="" alt="Course Image">
+        <div class='PCourses-Text'>
+            <p class='text-xl'>Learn C++</p>
+            <p class='mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?</p>
+        </div>
+    </div>
+    <div class='PCourses-Div'>
+        <img src="" alt="Course Image">
+        <div class='PCourses-Text'>
+            <p class='text-xl'>Learn C++</p>
+            <p class='mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?</p>
+        </div>
+    </div>
+    <div class='PCourses-Div'>
+        <img src="" alt="Course Image">
+        <div class='PCourses-Text'>
+            <p class='text-xl'>Learn C++</p>
+            <p class='mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?</p>
+        </div>
+    </div>
+    <div class='PCourses-Div'>
+        <img src="" alt="Course Image">
+        <div class='PCourses-Text'>
+            <p class='text-xl'>Learn C++</p>
+            <p class='mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?</p>
+        </div>
+    </div>
+    <div class='PCourses-Div'>
+        <img src="" alt="Course Image">
+        <div class='PCourses-Text'>
+            <p class='text-xl'>Learn C++</p>
+            <p class='mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?</p>
+        </div>
+    </div>
+</div>
