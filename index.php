dd9dbcb2bc9d7ccf3a58bc3a7c1f07b3e24a0a1d
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-18 15:09:35 +0530

    Connected MySQL Database with PHP

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "database"; 
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
