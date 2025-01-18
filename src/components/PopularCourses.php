652675b605664fc4ee0acb9f0201049aecc7160b
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-18 12:03:16 +0000

    Improvement: Component

--- src/components/PopularCourses.php
+++ src/components/PopularCourses.php
@@ -4,38 +4,58 @@
 
 <div class='PopularCourses flex flex-wrap justify-center p-10'>
     <div class='PCourses-Div'>
-        <img src="" alt="Course Image">
+        <img src="public/CSS.png" alt="Course Image">
         <div class='PCourses-Text'>
             <p class='text-xl'>Learn C++</p>
             <p class='mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?</p>
         </div>
+        <div class="Price">
+            <p>Rs. 100</p>
+            <button>Enroll</button>
+        </div>
     </div>
     <div class='PCourses-Div'>
-        <img src="" alt="Course Image">
+        <img src="public/CSS.png" alt="Course Image">
         <div class='PCourses-Text'>
             <p class='text-xl'>Learn C++</p>
             <p class='mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?</p>
         </div>
+        <div class="Price">
+            <p>Rs. 100</p>
+            <button>Enroll</button>
+        </div>
     </div>
     <div class='PCourses-Div'>
-        <img src="" alt="Course Image">
+        <img src="public/CSS.png" alt="Course Image">
         <div class='PCourses-Text'>
             <p class='text-xl'>Learn C++</p>
             <p class='mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?</p>
         </div>
+        <div class="Price">
+            <p>Rs. 100</p>
+            <button>Enroll</button>
+        </div>
     </div>
     <div class='PCourses-Div'>
-        <img src="" alt="Course Image">
+        <img src="public/CSS.png" alt="Course Image">
         <div class='PCourses-Text'>
             <p class='text-xl'>Learn C++</p>
             <p class='mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?</p>
         </div>
+        <div class="Price">
+            <p>Rs. 100</p>
+            <button>Enroll</button>
+        </div>
     </div>
     <div class='PCourses-Div'>
-        <img src="" alt="Course Image">
+        <img src="public/CSS.png" alt="Course Image">
         <div class='PCourses-Text'>
             <p class='text-xl'>Learn C++</p>
             <p class='mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?</p>
+        </div>
+        <div class="Price">
+            <p>Rs. 100</p>
+            <button>Enroll</button>
         </div>
     </div>
 </div>
