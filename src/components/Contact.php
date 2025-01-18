6ad968a3fa02f6d13951d6da6b17ef81526c7133
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-18 21:44:28 +0530

    Added: Component Contact

---
+++ src/components/Contact.php
@@ -0,0 +1,18 @@
+<head>
+    <link rel="stylesheet" href="src/styles/contact.css">
+</head>
+<div class='Contact'>
+    <h1 class='text-3xl text-center'>Contact</h1>
+    <form action="" method="get">
+        <div class='InputDiv flex flex-col'>
+            <input type="text" name="name" placeholder='Name'>
+        </div>
+        <div class='InputDiv flex flex-col'>
+            <input type="text" name="subject" placeholder='Subject'>
+        </div>
+        <div class='InputDiv flex flex-col'>
+            <input type="email" name="email" placeholder='Email'>
+        </div>
+        <button type="submit">Send</button>
+    </form>
+</div>
