6ba386eda2f746b4cc068d52ea9576f402206a14
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-18 12:17:10 +0000

    Added: CSS For Courses Page

--- courses.php
+++ courses.php
@@ -1,3 +1,17 @@
-<?php
-include 'src/components/Header.html';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Learning Platform - Courses</title>
+
+    <link rel="stylesheet" href="src/styles/main.css">
+</head>
+<body>
+    <?php include 'src/components/Header.html'; ?>
+    <main class='flex flex-col z-0 justify-center items-center mt-20'>
+        <h1 class='text-5xl'>Courses</h1>
+        <?php include 'src/components/PopularCourses.php'; ?>
+    </main>
+</body>
+</html>
