53ca2cbd94e7899dcbe9be37ed2ab1d19b163b09
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-18 15:16:08 +0530

    Removed Some Comments

--- index.php
+++ index.php
@@ -4,10 +4,8 @@ $username = "root";
 $password = "";
 $dbname = "database"; 
 
-// Create connection
 $conn = new mysqli($servername, $username, $password);
 
-// Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
