936a065ab8746d3fba848a0e0afb6c58ee3b9cf7
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-18 21:47:33 +0530

    Added: Contact Page

---
+++ contact.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Learning Platform - Contact</title>
+</head>
+<body>
+    <main class='flex flex-col justify-center items-center'>
+        <?php include 'src/components/Contact.php'; ?>
+    </main>
+</body>
+</html>
