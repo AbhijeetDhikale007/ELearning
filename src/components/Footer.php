8839a05f0ffc26a001c6c546a10958ef4cab645c
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-19 16:14:20 +0530

    Some Improvements

--- src/components/Footer.php
+++ src/components/Footer.php
@@ -1,22 +1,26 @@
+<?php
+include 'src/components/Icons.php';
+?>
+
 <head>
     <link rel="stylesheet" href="src/styles/footer.css">
 </head>
 
 <footer class='DetailsDiv bg-orange-600 flex justify-between items-center'>
-    <div>
+    <a href='' target='_blank'>
         <?php echo $Icon_Instagram; ?>
         <p>Instagram</p>
-    </div>
-    <div>
+    </a>
+    <a href='' target='_blank'>
         <?php echo $Icon_X; ?>
         <p>X</p>
-    </div>
-    <div>
+    </a>
+    <a href='' target='_blank'>
         <?php echo $Icon_Facebook; ?>
         <p>Facebook</p>
-    </div>
-    <div>
+    </a>
+    <a href='' target='_blank'>
         <?php echo $Icon_WhatsApp; ?>
         <p>WhatsApp</p>
-    </div>
+    </a>
 </footer>
