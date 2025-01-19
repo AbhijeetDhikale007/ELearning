a70c9c061790cf5bf953cae6bd5a82494d4f72ee
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-19 15:35:57 +0530

    Added: Two Components

---
+++ src/components/Login.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+
+<form action="" method="get">
+    <div>
+        <input type="email" name="email">
+    </div>
+    <div>
+        <input type="password" name="password">
+    </div>
+</form>
---
+++ src/components/Sign.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<form action="" method="get">
+    <div>
+        <input type="text" name="email" placeholder='Name'>
+    </div>
+    <div>
+        <input type="email" name="email" placeholder='Email'>
+    </div>
+    <div>
+        <input type="password" name="password" placeholder='Password'>
+    </div>
+    <div>
+        <input type="password" name="password" placeholder='Confirm Password'>
+    </div>
+</form>
