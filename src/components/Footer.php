0c66cc77ddf4646cde9e8cd652dfd3f728f82aea
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-19 16:04:47 +0530

    Added: Component Footer

---
+++ src/components/Footer.php
@@ -0,0 +1,22 @@
+<head>
+    <link rel="stylesheet" href="src/styles/footer.css">
+</head>
+
+<footer class='DetailsDiv bg-orange-600 flex justify-between items-center'>
+    <div>
+        <?php echo $Icon_Instagram; ?>
+        <p>Instagram</p>
+    </div>
+    <div>
+        <?php echo $Icon_X; ?>
+        <p>X</p>
+    </div>
+    <div>
+        <?php echo $Icon_Facebook; ?>
+        <p>Facebook</p>
+    </div>
+    <div>
+        <?php echo $Icon_WhatsApp; ?>
+        <p>WhatsApp</p>
+    </div>
+</footer>
