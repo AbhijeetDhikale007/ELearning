1b4a7d14990556891aa045f7fdbd073b5eef58ad
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-19 15:38:32 +0530

    Added: Placeholder

--- src/components/Login.php
+++ src/components/Login.php
@@ -4,9 +4,9 @@
 
 <form action="" method="get">
     <div>
-        <input type="email" name="email">
+        <input type="email" name="email" placeholder='Email'>
     </div>
     <div>
-        <input type="password" name="password">
+        <input type="password" name="password" placeholder='Password'>
     </div>
 </form>
