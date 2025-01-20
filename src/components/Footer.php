e9ce9ce92f4e035deab2dca3517a252a197abf29
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-20 15:57:08 +0530

    Improvement: Footer

--- src/components/Footer.php
+++ src/components/Footer.php
@@ -6,7 +6,7 @@ include 'src/components/Icons.php';
     <link rel="stylesheet" href="src/styles/footer.css">
 </head>
 
-<footer class='bg-orange-600 flex flex-col justify-between items-center'>
+<footer class='flex flex-col justify-between items-center'>
     <div class='DetailsDiv flex justify-between items-center w-100%'>
         <a href='' target='_blank'>
             <?php echo $Icon_Instagram; ?>
@@ -25,15 +25,22 @@ include 'src/components/Icons.php';
             <p>WhatsApp</p>
         </a>
     </div>
-    <div class='w-92% flex justify-between items-center'>
+    <div class='LinksDiv flex justify-between'>
         <div>
-            <h1>E-Learning</h1>
+            <h1 class='text-3xl'>E-Learning</h1>
         </div>
         <div>
             <h3>Links</h3>
+            <a class='Button' href="">Courses</a>
+            <a class='Button' href="">Contact</a>
         </div>
         <div>
             <h3>Get in touch</h3>
+            <a class='Button' href="">YouTube</a>
+            <a class='Button' href="">LinkedIn</a>
+            <a class='Button' href="">X</a>
+            <a class='Button' href="">Facebook</a>
+            <a class='Button' href="">Instagram</a>
         </div>
     </div>
 </footer>
