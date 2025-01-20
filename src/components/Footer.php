bfbac3a6ecb8820cc6d146af5e88cc27d282b267
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-20 15:14:37 +0530

    Improvement: Footer

--- src/components/Footer.php
+++ src/components/Footer.php
@@ -6,21 +6,26 @@ include 'src/components/Icons.php';
     <link rel="stylesheet" href="src/styles/footer.css">
 </head>
 
-<footer class='DetailsDiv bg-orange-600 flex justify-between items-center'>
-    <a href='' target='_blank'>
-        <?php echo $Icon_Instagram; ?>
-        <p>Instagram</p>
-    </a>
-    <a href='' target='_blank'>
-        <?php echo $Icon_X; ?>
-        <p>X</p>
-    </a>
-    <a href='' target='_blank'>
-        <?php echo $Icon_Facebook; ?>
-        <p>Facebook</p>
-    </a>
-    <a href='' target='_blank'>
-        <?php echo $Icon_WhatsApp; ?>
-        <p>WhatsApp</p>
-    </a>
+<footer class='bg-orange-600 flex flex-col justify-between items-center'>
+    <div class='DetailsDiv flex justify-between items-center w-100%'>
+        <a href='' target='_blank'>
+            <?php echo $Icon_Instagram; ?>
+            <p>Instagram</p>
+        </a>
+        <a href='' target='_blank'>
+            <?php echo $Icon_X; ?>
+            <p>X</p>
+        </a>
+        <a href='' target='_blank'>
+            <?php echo $Icon_Facebook; ?>
+            <p>Facebook</p>
+        </a>
+        <a href='' target='_blank'>
+            <?php echo $Icon_WhatsApp; ?>
+            <p>WhatsApp</p>
+        </a>
+    </div>
+    <div>
+
+    </div>
 </footer>
