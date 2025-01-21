c53340354eacee23c96f426e8d1c11b6581ed7c6
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-21 14:35:46 +0530

    Improvement: Header

--- src/components/Header.php
+++ src/components/Header.php
@@ -1,26 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-Learning Platform</title>
-
-    <link rel="stylesheet" href="src/styles/main.css">
-</head>
-<body>
-    <header class='flex absolute top-0 items-center justify-between'>
-        <p class="text-3xl select-none">E-Learning</p>
-        <nav class='flex justify-between items-center w-60% text-lg'>
-            <a class='Button' href="/Elearning/index.php">Home</a>
-            <a class='Button' href="/Elearning/cources.php">Course</a>
-            <a class='Button' href="/Elearning/contact.php">Contact</a>
-        </nav>
-        <nav class='flex gap-5 items-center'>
-                <a class='LoginButton' href="#">Login</a>
-                <a class='SignupButton' href="#">Signup</a>
-        </nav>
-    </header>
-    
-    <script src="src/js/jquery-3.7.1.js"></script>
-</body>
-</html>
+<header class='flex absolute top-0 items-center justify-between'>
+    <p class="text-3xl select-none">E-Learning</p>
+    <nav class='flex justify-between items-center w-60% text-lg'>
+        <a class='Button' href="/Elearning/index.php">Home</a>
+        <a class='Button' href="/Elearning/cources.php">Course</a>
+        <a class='Button' href="/Elearning/contact.php">Contact</a>
+    </nav>
+    <nav class='flex gap-5 items-center'>
+            <a class='LoginButton' href="#">Login</a>
+            <a class='SignupButton' href="#">Signup</a>
+    </nav>
+</header>
