b14c830a62c94922b920769ce026aa436aeca1fd
https://github.com/AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-21 18:29:58 +0530

    Improvement: Contact Component

--- src/components/Contact.php
+++ src/components/Contact.php
@@ -1,8 +1,14 @@
 <head>
     <link rel="stylesheet" href="src/styles/contact.css">
 </head>
-<div class='Contact'>
-    <h1 class='text-3xl text-center'>Contact</h1>
+
+<div class='ContactContainer flex justify-center'>
+<div class='Contact flex justify-center'>
+    <div>
+        <h1 class='text-6xl text-center'>Contact Us</h1>
+        <p class='mb-10'>Get in touch with any questions or thoughts you have about Deep Learning Courses.</p>
+        <a class="MailLink text-xl" target="_blank" href="">info@elearning.com</a>
+    </div>
     <form action="" method="get">
         <div class='InputDiv flex flex-col'>
             <input type="text" name="name" placeholder='Name'>
@@ -13,6 +19,10 @@
         <div class='InputDiv flex flex-col'>
             <input type="email" name="email" placeholder='Email'>
         </div>
+        <div class='InputDiv flex flex-col'>
+            <textarea name="Message" placeholder='Your Message'></textarea>
+        </div>
         <button class='FormButton' type="submit">Send</button>
     </form>
+</div>
 </div>
