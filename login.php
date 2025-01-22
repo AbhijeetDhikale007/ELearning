546896a91de17f3d8664a34f40e603f652d297be
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-22 16:01:53 +0530

    Major Improvement

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+// include 'src/components/dBconnection.php';
+include 'src/components/Icons.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Learning Platform - Login</title>
+
+    <link rel="stylesheet" href="src/styles/main.css">
+    <link rel="stylesheet" href="src/styles/login.css">
+</head>
+<body>
+    <?php echo include 'src/components/Header.php'; ?>
+
+    <main class='flex flex-col z-0 justify-center items-center'>
+        <?php include 'src/components/Login.php'; ?>
+    </main>
+
+    <script src="src/js/jquery-3.7.1.js"></script>
+</body>
+</html>
--- src/components/Login.php
+++ src/components/Login.php
@@ -2,11 +2,18 @@
 
 ?>
 
-<form action="" method="get">
-    <div>
-        <input type="email" name="email" placeholder='Email'>
-    </div>
-    <div>
-        <input type="password" name="password" placeholder='Password'>
-    </div>
-</form>
+<div class='Login'>
+    <h1 class='Heading'>Welcome Back</h1>
+    <form action="" method="get">
+        <div>
+            <label for="email">Email</label>
+            <input type="email" name="email">
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input type="password" name="password">
+        </div>
+        <a href="">Forgot Your Password?</a>
+        <button class='FormButton' type="submit">Login</button>
+    </form>
+</div>
