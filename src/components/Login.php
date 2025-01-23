1226c532ea9a74b0603a6e4dcd5895f95b72a46e
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-23 16:04:59 +0530

    Improvement: Login

--- src/components/Login.php
+++ src/components/Login.php
@@ -5,15 +5,18 @@
 <div class='Login'>
     <h1 class='Heading'>Welcome Back</h1>
     <form action="" method="get">
-        <div>
+        <div class='flex w-100 flex-col relative items-center justify-center'>
             <label for="email">Email</label>
             <input type="email" name="email" required>
         </div>
-        <div>
+        <div class='flex w-100 flex-col relative items-center justify-center'>
             <label for="password">Password</label>
             <input type="password" name="password" required>
         </div>
         <a href="">Forgot Your Password?</a>
-        <button class='FormButton' type="submit">Login</button>
+        <div class='flex flex-row justify-between w-100 items-center'>
+            <button class='FormButton' type="submit">Login</button>
+            <a href="/adminlogin">Admin Login</a>
+        </div>
     </form>
 </div>
