80baccd499a7e5aeaf7295161272943dc60b2440
https://github.com/AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-24 15:14:11 +0530

    CSS Improvement

--- src/components/Header.php
+++ src/components/Header.php
@@ -1,5 +1,5 @@
 <header class='flex absolute top-0 items-center justify-between'>
-    <p class="text-3xl select-none">E-Learning</p>
+    <p class="Brand text-3xl select-none">E-Learning</p>
     <nav class='flex justify-between items-center w-60% text-lg'>
         <a class='Button' href="/Elearning/index.php">Home</a>
         <a class='Button' href="/Elearning/courses.php">Courses</a>
