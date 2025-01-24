7c50e10ee70387323fbaadd1b7212a2541412bc3
https://github.com/AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-24 13:10:33 +0530

    Added: Page

---
+++ signup.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Learning Platform - Sign Up</title>
+
+    <link rel="stylesheet" href="src/styles/main.css">
+    <link rel="stylesheet" href="src/styles/signup.css">
+</head>
+<body>
+    <?php echo include 'src/components/Header.php';?>
+
+    <main class='flex flex-col z-0 justify-center items-center'>
+        <?php include 'src/components/SignUp.php'; ?>
+    </main>
+
+    <script src="src/js/jquery-3.7.1.js"></script>
+</body>
+</html>
