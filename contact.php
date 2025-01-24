31b83cabd3b30e87245f17a8136fad0e24295b38
https://github.com/AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-24 15:53:41 +0530

    Added: Page Icon

--- contact.php
+++ contact.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-Learning Platform - Contact</title>
+    <link rel="icon" type='img/png' href="public/Logo.png">
+
     <link rel="stylesheet" href="src/styles/contact.css">
 </head>
 <body>
