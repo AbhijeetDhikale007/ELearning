d6afc63c9850eecbfc5bd7ddfcdb0b94ce54dff0
https://github.com/AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-26 14:14:45 +0530

    Added: Courses Data

--- src/components/PopularCourses.php
+++ src/components/PopularCourses.php
@@ -1,8 +1,8 @@
 <?php
 $Courses = [
-    [ 'img' => 'public/CSS.png', 'Name' => 'Learn C++', 'Info' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?", 'Prize' => "Rs. 100" ],
-    [ 'img' => 'public/CSS.png', 'Name' => 'Learn C++', 'Info' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?", 'Prize' => "Rs. 100" ],
-    [ 'img' => 'public/CSS.png', 'Name' => 'Learn C++', 'Info' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?", 'Prize' => "Rs. 100" ],
+    [ 'img' => 'public/CSS.png', 'Name' => 'Data Science: Transformers for Natural Language Processing', 'Info' => "ChatGPT, GPT-4, BERT, Deep Learning, Machine Learning, & NLP with Hugging Face, Attention in Python, Tensorflow, PyTorch", 'Prize' => "Rs. 100" ],
+    [ 'img' => 'public/CSS.png', 'Name' => 'Math 0-1: Calculus for Data Science & Machine Learning', 'Info' => "A Casual Guide for Artificial Intelligence, Deep Learning, and Python Programmers", 'Prize' => "Rs. 100" ],
+    [ 'img' => 'public/CSS.png', 'Name' => 'Financial Engineering and Artificial Intelligence in Python', 'Info' => "Financial Analysis, Time Series Analysis, Portfolio Optimization, CAPM, Algorithmic Trading, Q-Learning, and MORE!", 'Prize' => "Rs. 100" ],
     [ 'img' => 'public/CSS.png', 'Name' => 'Learn C++', 'Info' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?", 'Prize' => "Rs. 100" ],
 ];
 ?>
