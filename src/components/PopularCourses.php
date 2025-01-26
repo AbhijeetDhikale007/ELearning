fa66439f7517c720fb3f7248849a9184375703db
https://github.com/AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-26 14:09:14 +0530

    Made Dynamic With Multidimentional Array

--- src/components/PopularCourses.php
+++ src/components/PopularCourses.php
@@ -1,57 +1,24 @@
+<?php
+$Courses = [
+    [ 'img' => 'public/CSS.png', 'Name' => 'Learn C++', 'Info' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?", 'Prize' => "Rs. 100" ],
+    [ 'img' => 'public/CSS.png', 'Name' => 'Learn C++', 'Info' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?", 'Prize' => "Rs. 100" ],
+    [ 'img' => 'public/CSS.png', 'Name' => 'Learn C++', 'Info' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?", 'Prize' => "Rs. 100" ],
+    [ 'img' => 'public/CSS.png', 'Name' => 'Learn C++', 'Info' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?", 'Prize' => "Rs. 100" ],
+];
+?>
+
 <div class='PopularCourses flex flex-wrap justify-center p-10'>
-    <div class='PCourses-Div'>
-        <img src="public/CSS.png" alt="Course Image">
-        <div class='PCourses-Text'>
-            <p class='text-xl'>Learn C++</p>
-            <p class='mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?</p>
+    <?php foreach($Courses as $courses): ?>
+        <div class='PCourses-Div'>
+            <img src="<?= $courses['img'] ?>" alt="Course Image">
+            <div class='PCourses-Text'>
+                <p class='text-xl'><?= $courses['Name'] ?></p>
+                <p class='mt-2'><?= $courses['Info'] ?></p>
+            </div>
+            <div class="Price">
+                <p><?= $courses['Prize'] ?></p>
+                <button>Enroll</button>
+            </div>
         </div>
-        <div class="Price">
-            <p>Rs. 100</p>
-            <button>Enroll</button>
-        </div>
-    </div>
-    <div class='PCourses-Div'>
-        <img src="public/CSS.png" alt="Course Image">
-        <div class='PCourses-Text'>
-            <p class='text-xl'>Learn C++</p>
-            <p class='mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?</p>
-        </div>
-        <div class="Price">
-            <p>Rs. 100</p>
-            <button>Enroll</button>
-        </div>
-    </div>
-    <div class='PCourses-Div'>
-        <img src="public/CSS.png" alt="Course Image">
-        <div class='PCourses-Text'>
-            <p class='text-xl'>Learn C++</p>
-            <p class='mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?</p>
-        </div>
-        <div class="Price">
-            <p>Rs. 100</p>
-            <button>Enroll</button>
-        </div>
-    </div>
-    <div class='PCourses-Div'>
-        <img src="public/CSS.png" alt="Course Image">
-        <div class='PCourses-Text'>
-            <p class='text-xl'>Learn C++</p>
-            <p class='mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?</p>
-        </div>
-        <div class="Price">
-            <p>Rs. 100</p>
-            <button>Enroll</button>
-        </div>
-    </div>
-    <div class='PCourses-Div'>
-        <img src="public/CSS.png" alt="Course Image">
-        <div class='PCourses-Text'>
-            <p class='text-xl'>Learn C++</p>
-            <p class='mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?</p>
-        </div>
-        <div class="Price">
-            <p>Rs. 100</p>
-            <button>Enroll</button>
-        </div>
-    </div>
+    <?php endforeach ?>
 </div>
