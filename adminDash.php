e488e1e9a2e0fdb967b76606746df010c85cb8a4
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-28 18:14:28 +0530

    Minor Improvement

--- adminDash.php
+++ adminDash.php
@@ -15,7 +15,7 @@ include 'src/components/Icons.php';
     <link rel="stylesheet" href="src/styles/adminDash.css">
 </head>
 <body class='bg-whitish'>
-    <main class='flex z-0 items-center'>
+    <main class='flex z-0 items-center gap-20px'>
         <?php include 'src/components/AdminDash.php'; ?>
     </main>
 
