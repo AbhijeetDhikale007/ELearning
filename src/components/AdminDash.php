91ef58854b9c4ef853dd529d481350872ac71fad
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-28 21:02:26 +0530

    Minor Improvement

--- src/components/AdminDash.php
+++ src/components/AdminDash.php
@@ -32,8 +32,9 @@ include 'Icons.php';
     <div>
         <div class='Profile'>
             <img src="public/Profile.jpg" alt="">
-            <div class='h-80'>
+            <div class='h-80 flex flex-col gap-20px'>
                 <p>Abhijeet Dhikale</p>
+                <p class='text-0-6'>Instructor/Admin</p>
             </div>
         </div>
     </div>
