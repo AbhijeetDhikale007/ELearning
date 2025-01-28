9753d355ff0a3b339eef5a0dfcf73e02aa5e2223
https://github.com/AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-28 12:35:26 +0530

    Added: Admin Dashboard

---
+++ adminDash.php
@@ -0,0 +1,26 @@
+<?php
+// include 'src/components/dBconnection.php';
+include 'src/components/Icons.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Learning Platform - Admin Dashboard</title>
+    <link rel="icon" type='img/png' href="public/Logo.png">
+
+    <link rel="stylesheet" href="src/styles/main.css">
+    <link rel="stylesheet" href="src/styles/adminDash.css">
+</head>
+<body>
+    <?php include 'src/components/Header.php'; ?>
+
+    <main class='flex flex-col z-0 justify-center items-center'>
+        <?php include 'src/components/AdminLogin.php'; ?>
+    </main>
+
+    <script src="src/js/jquery-3.7.1.js"></script>
+</body>
+</html>
