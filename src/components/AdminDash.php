253791bbfd3e1826fa4b80adf8efbb6b2ecf73ea
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-29 17:29:03 +0530

    Minor Changes

--- src/components/AdminDash.php
+++ src/components/AdminDash.php
@@ -13,10 +13,13 @@ $ImgLink = 'public/Profile.jpg';
     <div>
         <div class='Profile'>
             <img src=<?php echo $ImgLink; ?> alt="Profile">
-            <div class='h-80 flex flex-col gap-20px'>
+            <div class='h-100 flex flex-col gap-20px'>
                 <p><?php echo $Name; ?></p>
                 <p class='text-0-6'>Instructor/Admin</p>
                 <p class='text-0-6'><?php echo $Username; ?></p>
+                <div class='flex justify-end items-end'>
+                    <button><?php echo $Icon_Edit ?></button>
+                </div>
             </div>
         </div>
     </div>
