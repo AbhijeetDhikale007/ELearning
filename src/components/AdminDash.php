3dc21a1f002bb984dfaef2654a788a34c244eec4
https://github.com/AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-29 12:10:42 +0530

    Improvement

--- src/components/AdminDash.php
+++ src/components/AdminDash.php
@@ -3,30 +3,7 @@
 include 'Icons.php';
 ?>
 
-<aside class='flex justify-center items-center'>
-    <div class='flex flex-col gap-16px justify-center'>
-        <a href="">
-            <?php echo $Icon_Dashboard ?>
-            Dashboard
-        </a>
-        <a href="">
-            <?php echo $Icon_Courses ?>
-            Courses
-        </a>
-        <a href="">
-            <?php echo $Icon_Students ?>
-            Students
-        </a>
-        <a href="">
-
-
-        </a>
-        <a href="">
-
-
-        </a>
-    </div>
-</aside>
+<?php include 'adminAside.php'; ?>
 <div class='DashContent w-100%'>
     <h1 class='arsenal-sc'>Dashboard</h1>
     <div>
---
+++ src/components/adminAside.php
@@ -0,0 +1,29 @@
+<?php
+include 'Icons.php';
+?>
+
+<aside class='flex justify-center items-center'>
+    <div class='flex flex-col gap-16px justify-center relative text-white'>
+        <p class='absolute top-4per w-100 text-1-4 text-white text-center'>E-Learning</p>
+        <a href="">
+            <?php echo $Icon_Dashboard ?>
+            Dashboard
+        </a>
+        <a href="">
+            <?php echo $Icon_Courses ?>
+            Courses
+        </a>
+        <a href="">
+            <?php echo $Icon_Students ?>
+            Students
+        </a>
+        <!-- <a href="">
+
+
+        </a>
+        <a href="">
+
+
+        </a> -->
+    </div>
+</aside>
