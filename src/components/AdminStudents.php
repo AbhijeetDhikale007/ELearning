ffc90860a314c056e09cdf4521a3d6215f682321
https://github.com/AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-29 13:43:39 +0530

    Minor Improvement

--- src/components/AdminStudents.php
+++ src/components/AdminStudents.php
@@ -4,7 +4,8 @@ include 'Icons.php';
 ?>
 
 <?php include 'AdminAside.php'; ?>
-<div class='DashContent w-100%'>
+<div class='DashContent w-100% relative'>
     <h1 class='arsenal-sc'>Students</h1>
+    <button class='Button-AddCourse absolute'>Add Course +</button>
 
 </div>
