46bcedebbd70efbd47e781800d8c6d536e6c57a9
https://github.com/AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-29 12:25:16 +0530

    Minor Improvement

--- src/components/adminAside.php
+++ src/components/adminAside.php
@@ -5,23 +5,23 @@ include 'Icons.php';
 <aside class='flex justify-center items-center'>
     <div class='flex flex-col gap-16px justify-center relative text-white'>
         <p class='absolute top-4per w-100 text-1-4 text-white text-center'>E-Learning</p>
-        <a href="">
+        <a href="" class:active={}>
             <?php echo $Icon_Dashboard ?>
             Dashboard
         </a>
-        <a href="">
+        <a href="" class:active={}>
             <?php echo $Icon_Courses ?>
             Courses
         </a>
-        <a href="">
+        <a href="" class:active={}>
             <?php echo $Icon_Students ?>
             Students
         </a>
-        <!-- <a href="">
+        <!-- <a href="" class:active={}>
 
 
         </a>
-        <a href="">
+        <a href="" class:active={}>
 
 
         </a> -->
