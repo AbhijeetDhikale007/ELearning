6b0f580e3c7e6a70e5602edb692732222420b7f1
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-29 18:18:43 +0530

    Improvement

---
+++ src/components/StudentAside.php
@@ -0,0 +1,25 @@
+<?php
+include 'Icons.php';
+?>
+
+<aside class='flex justify-center items-center'>
+    <div class='flex flex-col gap-16px justify-center relative text-white'>
+        <p class='absolute top-4per w-100 text-1-4 text-white text-center'>E-Learning</p>
+        <a href="studentDash.php" class:active={}>
+            <?php echo $Icon_Dashboard ?>
+            Dashboard
+        </a>
+        <a href="studentCourses.php" class:active={}>
+            <?php echo $Icon_Courses ?>
+            Courses
+        </a>
+        <!-- <a href="" class:active={}>
+
+
+        </a>
+        <a href="" class:active={}>
+
+
+        </a> -->
+    </div>
+</aside>
--- src/components/StudentDash.php
+++ src/components/StudentDash.php
@@ -1,29 +1,26 @@
 <?php
+// include 'src/components/dBconnection.php';
 include 'Icons.php';
+
+$Name = 'Your Name';
+$Username = 'Username';
+$ImgLink = 'public/Profile.jpg';
 ?>
 
-<aside class='flex justify-center items-center'>
-    <div class='flex flex-col gap-16px justify-center relative text-white'>
-        <p class='absolute top-4per w-100 text-1-4 text-white text-center'>E-Learning</p>
-        <a href="adminDash.php" class:active={}>
-            <?php echo $Icon_Dashboard ?>
-            Dashboard
-        </a>
-        <a href="adminCourses.php" class:active={}>
-            <?php echo $Icon_Courses ?>
-            Courses
-        </a>
-        <a href="adminStudents.php" class:active={}>
-            <?php echo $Icon_Students ?>
-            Students
-        </a>
-        <!-- <a href="" class:active={}>
-
-
-        </a>
-        <a href="" class:active={}>
-
-
-        </a> -->
+<?php include 'StudentAside.php'; ?>
+<div class='DashContent w-100%'>
+    <h1 class='arsenal-sc'>Student Dashboard</h1>
+    <div>
+        <div class='Profile'>
+            <img src=<?php echo $ImgLink; ?> alt="Profile">
+            <div class='h-100 flex flex-col gap-20px'>
+                <p><?php echo $Name; ?></p>
+                <p class='text-0-6'>Student</p>
+                <p class='text-0-6'><?php echo $Username; ?></p>
+                <div class='flex justify-end items-end'>
+                    <button><?php echo $Icon_Edit ?></button>
+                </div>
+            </div>
+        </div>
     </div>
-</aside>
+</div>
