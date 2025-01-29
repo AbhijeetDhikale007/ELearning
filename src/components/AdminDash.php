4e57ef243c64e10fc3dfe03a2128098354ea288d
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-29 16:20:00 +0530

    Minor Changes

--- src/components/AdminDash.php
+++ src/components/AdminDash.php
@@ -1,6 +1,10 @@
 <?php
 // include 'src/components/dBconnection.php';
 include 'Icons.php';
+
+$Name = 'Your Name';
+$Username = 'Username';
+$ImgLink = 'public/Profile.jpg';
 ?>
 
 <?php include 'AdminAside.php'; ?>
@@ -8,10 +12,11 @@ include 'Icons.php';
     <h1 class='arsenal-sc'>Dashboard</h1>
     <div>
         <div class='Profile'>
-            <img src="public/Profile.jpg" alt="">
+            <img src=<?php echo $ImgLink; ?> alt="Profile">
             <div class='h-80 flex flex-col gap-20px'>
-                <p>Your Name</p>
+                <p><?php echo $Name; ?></p>
                 <p class='text-0-6'>Instructor/Admin</p>
+                <p class='text-0-6'><?php echo $Username; ?></p>
             </div>
         </div>
     </div>
