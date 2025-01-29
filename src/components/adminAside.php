85862a6b53bf926aa2b7a4a1d1100187475d2521
https://github.com/AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-29 13:18:05 +0530

    Added: One More Page

--- src/components/adminAside.php
+++ src/components/adminAside.php
@@ -5,15 +5,15 @@ include 'Icons.php';
 <aside class='flex justify-center items-center'>
     <div class='flex flex-col gap-16px justify-center relative text-white'>
         <p class='absolute top-4per w-100 text-1-4 text-white text-center'>E-Learning</p>
-        <a href="" class:active={}>
+        <a href="adminDash.php" class:active={}>
             <?php echo $Icon_Dashboard ?>
             Dashboard
         </a>
-        <a href="AdminCourses.php" class:active={}>
+        <a href="adminCourses.php" class:active={}>
             <?php echo $Icon_Courses ?>
             Courses
         </a>
-        <a href="" class:active={}>
+        <a href="adminStudents.php" class:active={}>
             <?php echo $Icon_Students ?>
             Students
         </a>
