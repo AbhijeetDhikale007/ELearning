ffb0dee2234c350962a64f596765650f85f663a4
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-29 18:30:33 +0530

    Minor Changes

---
+++ src/components/StudentCourses.php
@@ -0,0 +1,30 @@
+<?php
+// include 'src/components/dBconnection.php';
+include 'Icons.php';
+
+$Courses = [
+    [ 'img' => 'public/CSS.png', 'Name' => 'Data Science: Transformers for Natural Language Processing', 'Info' => "ChatGPT, GPT-4, BERT, Deep Learning, Machine Learning, & NLP with Hugging Face, Attention in Python, Tensorflow, PyTorch", 'Prize' => "Rs. 100" ],
+    [ 'img' => 'public/CSS.png', 'Name' => 'Math 0-1: Calculus for Data Science & Machine Learning', 'Info' => "A Casual Guide for Artificial Intelligence, Deep Learning, and Python Programmers", 'Prize' => "Rs. 100" ],
+    [ 'img' => 'public/CSS.png', 'Name' => 'Financial Engineering and Artificial Intelligence in Python', 'Info' => "Financial Analysis, Time Series Analysis, Portfolio Optimization, CAPM, Algorithmic Trading, Q-Learning, and MORE!", 'Prize' => "Rs. 100" ],
+    [ 'img' => 'public/CSS.png', 'Name' => 'C++ Crash Course', 'Info' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, corporis?", 'Prize' => "Rs. 100" ],
+];
+?>
+
+<?php include 'StudentAside.php'; ?>
+<div class='DashContent w-100% relative'>
+    <h1 class='arsenal-sc'>Courses</h1>
+    <!-- <div class='flex justify-end w-94'>
+        <button class='Button-AddCourse'>Add Course +</button>
+    </div> -->
+    <div class='flex flex-col gap-16px w-94'>
+        <?php foreach($Courses as $courses): ?>
+            <div class='CourseContainer flex justify-between p-3'>
+                <p><?= $courses['Name'] ?></p>
+                <div class='flex gap-15px'>
+                    <!-- <button><?php echo $Icon_Delete ?></button>
+                    <button><?php echo $Icon_Edit ?></button> -->
+                </div>
+            </div>
+        <?php endforeach ?>
+    </div>
+</div>
