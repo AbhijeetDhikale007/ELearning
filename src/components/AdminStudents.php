f2951639421be1624c9bd51706527d1e42a9b049
https://github.com/AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-29 13:19:44 +0530

    Imported Aside

---
+++ src/components/AdminStudents.php
@@ -0,0 +1,10 @@
+<?php
+// include 'src/components/dBconnection.php';
+include 'Icons.php';
+?>
+
+<?php include 'AdminAside.php'; ?>
+<div class='DashContent w-100%'>
+    <h1 class='arsenal-sc'>Students</h1>
+
+</div>
