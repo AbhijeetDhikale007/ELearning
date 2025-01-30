616349dfa04b0ff9527179c8b3f39531f9d325c8
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-01-30 14:27:08 +0530

    Added: More Student Info

--- src/components/StudentProfile.php
+++ src/components/StudentProfile.php
@@ -3,9 +3,11 @@
 include 'Icons.php';
 
 $Name = 'Your Name';
-$Username = 'Username';
+$Email = 'xyz.cct@gmail.com';
 $ImgLink = 'public/Profile.jpg';
 $Num = '+91 88056 47422';
+$Address = 'House No. 108, Near College, Nashik, Maharashtra ';
+$College = 'MET Bhujbal Knowledge City';
 ?>
 
 <?php include 'StudentAside.php'; ?>
@@ -14,11 +16,13 @@ $Num = '+91 88056 47422';
     <div>
         <div class='Profile'>
             <img src=<?php echo $ImgLink; ?> alt="Profile">
-            <div class='h-100 flex flex-col gap-20px'>
+            <div class='h-100 flex flex-col gap-16px'>
                 <p><?php echo $Name; ?></p>
                 <p class='text-0-6'>Student</p>
-                <p class='text-0-6'><?php echo $Username; ?></p>
+                <p class='text-0-6'><?php echo $Email; ?></p>
                 <p class='text-0-48'><?php echo $Num; ?></p>
+                <p class='text-0-48'><?php echo $Address; ?></p>
+                <p class='text-0-48'><?php echo $College; ?></p>
                 <div class='flex justify-end items-end'>
                     <button><?php echo $Icon_Edit ?></button>
                 </div>
