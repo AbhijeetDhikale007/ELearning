77a3d90158101e0c220ff045e1945416952f5674
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-02-11 13:07:34 +0530

    Added: Remote Connection To The Database

--- src/components/dBconnection.php
+++ src/components/dBconnection.php
@@ -1,8 +1,10 @@
+<!--------- Remote Connection --------->
+
 <?php
-$servername = "localhost";  // IP Address To Connect To The Database
-$username = "root";
-$password = "";
-$dbname = "elearning"; 
+$servername = "127.0.0.1";
+$username = "remote_user";
+$password = "_PassWord@cct";
+$dbname = "elearning";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -10,3 +12,18 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
+<!--------- Local Connection --------->
+
+<!-- <?php
+// $servername = "localhost";  // IP Address To Connect To The Database
+// $username = "root";
+// $password = "";
+// $dbname = "elearning"; 
+
+// $conn = new mysqli($servername, $username, $password, $dbname);
+
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+?> -->
