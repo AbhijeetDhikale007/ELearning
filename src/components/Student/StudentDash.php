42720e876f547ad6dab1a73e14f9e057ea313669
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-02-21 11:50:11 +0530

    Removed Dashboard Content and It's Button

--- src/components/Student/StudentDash.php
+++ src/components/Student/StudentDash.php
@@ -9,10 +9,6 @@ include 'src/components/Icons.php';
 <aside class='fixed top-0 flex justify-center items-center w-[18vw] md:w-[18vw]'>
     <div class='flex flex-col gap-16px justify-center relative text-white'>
         <p class='josefin-sans absolute text-[2.2vw] top-4per w-100 text-black text-center'>E-Learning</p>
-        <button id='DashButton' class:active={}>
-            <?php echo $Icon_Dashboard ?>
-            Dashboard
-        </button>
         <button id='ProfileButton' class:active={}>
             <?php echo $Icon_Profile ?>
             Profile
@@ -21,28 +17,14 @@ include 'src/components/Icons.php';
             <?php echo $Icon_Courses ?>
             Courses
         </button>
-        <!-- <a href="" class:active={}>
-
-
-        </a>
-        <a href="" class:active={}>
-
-
-        </a> -->
         <a href='index.php' class='absolute bottom-4per w-100 text-1-3 text-white flex justify-center items-center'><?php echo $Icon_Back; ?></a>
     </div>
 </aside>
 </section>
 
-<!------------- Dashboard Content ------------->
-
-<div class='DashContent w-100%' id='DashContent'>
-    <h1 class='josefin-sans'>Dashboard</h1>
-</div>
-
 <!------------- Profile Content ------------->
 
-<div class='DashContent w-100%' id='ProfileContent'>
+<div class='DashContent Active w-100%' id='ProfileContent'>
     <?php include 'src/components/Student/StudentProfile.php'; ?>
 </div>
 
@@ -53,10 +35,6 @@ include 'src/components/Icons.php';
 </div>
 
 <script>
-    // Dashboard Button and Content
-    const DashButton = document.querySelector('#DashButton');
-    const DashContent = document.querySelector('#DashContent');
-
     // Profile Button and Content
     const ProfileButton = document.querySelector('#ProfileButton');
     const ProfileContent = document.querySelector('#ProfileContent');
@@ -65,29 +43,17 @@ include 'src/components/Icons.php';
     const CoursesButton = document.querySelector('#CoursesButton');
     const CoursesContent = document.querySelector('#CoursesContent');
 
-    // Dasboard Button
-    DashButton.addEventListener('click', ()=> {
-        DashContent.classList.remove('Active');
-        ProfileContent.classList.remove('Active');
-        CoursesContent.classList.remove('Active');
-
-        document.title = 'E-Learning Platform - Dashboard';
-    });
-
     // Profile Button
     ProfileButton.addEventListener('click', ()=> {
         ProfileContent.classList.add('Active');
-        DashContent.classList.add('Active');
         CoursesContent.classList.remove('Active');
 
         document.title = 'E-Learning Platform - Profile';
-
     });
 
     // Courses Button
     CoursesButton.addEventListener('click', ()=> {
         CoursesContent.classList.add('Active');
-        DashContent.classList.add('Active');
         ProfileContent.classList.remove('Active');
 
         document.title = 'E-Learning Platform - Courses';
