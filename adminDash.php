fcfae1bf0f063aea9ec8a1ed3349fdfdc1be8c7d
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-02-21 11:50:56 +0530

    Added JQuery CDN

--- adminDash.php
+++ adminDash.php
@@ -23,7 +23,7 @@ $Login_id = 1;
         <?php include 'src/components/Admin/AdminDash.php'; ?>
     </main>
 
-    <script src="src/js/jquery-3.7.1.js"></script>
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> -->
+    <!-- <script src="src/js/jquery-3.7.1.js"></script> -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 </html>
