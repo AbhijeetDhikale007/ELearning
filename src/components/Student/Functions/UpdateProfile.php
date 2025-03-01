e1c297203ea7fc11d66affd9a6eb0a83e2951fc4
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-03-01 13:19:02 +0530

    Added: Function To Update Particular Data or All

--- src/components/Student/Functions/UpdateProfile.php
+++ src/components/Student/Functions/UpdateProfile.php
@@ -5,21 +5,73 @@ $DB_file = 'C:\xampp\htdocs\ELearning\src\components\dBconnection.php';
 require $Login_file;
 require $DB_file;
 
-if(isset($_POST['editprofilelink']) && isset($_POST['editname']) && isset($_POST['editemail']) && isset($_POST['editnumber']) && isset($_POST['editlocation']) && isset($_POST['editcollege'])) {
+$Message = "Failed To Update Your Profile";
+
+if(isset($_POST['editprofilelink']) && !empty($_POST['editprofilelink'])) {
     
     $editprofilelink = $_POST['editprofilelink'];
-    $editname = $_POST['editname'];
-    $editemail = $_POST['editemail'];
-    $editnumber = $_POST['editnumber'];
-    $editlocation = $_POST['editlocation'];
-    $editcollege = $_POST['editcollege'];
     
-    $sql = "UPDATE students SET profileurl = '$editprofilelink', sname = '$editname', email = '$editemail', number = '$editnumber', address = '$editlocation', college = '$editcollege' WHERE id = '$Login_id'";
+    $sql = "UPDATE students SET profileurl = '$editprofilelink' WHERE id = '$Login_id'";
     
     if($conn->query($sql) == true) {
-        echo "Your Profile Updated Successfully";
-    } else {
-        echo "Failed To Update Your Profile";
+        $Message = "Your Profile Updated Successfully";
     }
 }
+
+if(isset($_POST['editname']) && !empty($_POST['editname'])) {
+    
+    $editname = $_POST['editname'];
+    
+    $sql = "UPDATE students SET sname = '$editname' WHERE id = '$Login_id'";
+    
+    if($conn->query($sql) == true) {
+        $Message = "Your Profile Updated Successfully";
+    }
+}
+
+if(isset($_POST['editemail']) && !empty($_POST['editemail'])) {
+    
+    $editemail = $_POST['editemail'];
+    
+    $sql = "UPDATE students SET email = '$editemail' WHERE id = '$Login_id'";
+    
+    if($conn->query($sql) == true) {
+        $Message = "Your Profile Updated Successfully";
+    }
+}
+
+if(isset($_POST['editnumber']) && !empty($_POST['editnumber'])) {
+    
+    $editnumber = $_POST['editnumber'];
+    
+    $sql = "UPDATE students SET number = '$editnumber' WHERE id = '$Login_id'";
+    
+    if($conn->query($sql) == true) {
+        $Message = "Your Profile Updated Successfully";
+    }
+}
+
+if(isset($_POST['editlocation']) && !empty($_POST['editlocation'])) {
+    
+    $editlocation = $_POST['editlocation'];
+    
+    $sql = "UPDATE students SET address = '$editlocation' WHERE id = '$Login_id'";
+    
+    if($conn->query($sql) == true) {
+        $Message = "Your Profile Updated Successfully";
+    }
+}
+
+if(isset($_POST['editcollege']) && !empty($_POST['editcollege'])) {
+    
+    $editcollege = $_POST['editcollege'];
+    
+    $sql = "UPDATE students SET college = '$editcollege' WHERE id = '$Login_id'";
+    
+    if($conn->query($sql) == true) {
+        $Message = "Your Profile Updated Successfully";
+    }
+}
+
+echo $Message;
 ?> 
