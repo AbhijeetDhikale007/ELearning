7e460fd9f88660a723c98125a6a44427a853134e
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-03-01 12:21:15 +0530

    Added: Function To Update All or Only Specific Data

--- src/components/Admin/Functions/UpdateProfile.php
+++ src/components/Admin/Functions/UpdateProfile.php
@@ -5,18 +5,40 @@ $DB_file = 'C:\xampp\htdocs\ELearning\src\components\dBconnection.php';
 require $Login_file;
 require $DB_file;
 
-if(isset($_POST['editprofilelink']) && isset($_POST['editname']) && isset($_POST['editusername'])) {
-    
+$Message = "Failed To Update Your Profile";
+
+if(isset($_POST['editprofilelink']) && !empty($_POST['editprofilelink'])) {
+
     $editprofilelink = $_POST['editprofilelink'];
-    $editname = $_POST['editname'];
-    $editusername = $_POST['editusername'];
     
-    $sql = "UPDATE admin SET profilelink = '$editprofilelink', name = '$editname', username = '$editusername' WHERE id = '$Login_id'";
+    $sql = "UPDATE admin SET profilelink = '$editprofilelink' WHERE id = '$Login_id'";
     
     if($conn->query($sql) == true) {
-        echo "Your Profile Updated Successfully";
-    } else {
-        echo "Failed To Update Your Profile";
+        $Message = "Your Profile Updated Successfully";
     }
 }
+
+if(isset($_POST['editname']) && !empty($_POST['editname'])) {
+
+    $editname = $_POST['editname'];
+    
+    $sql = "UPDATE admin SET name = '$editname' WHERE id = '$Login_id'";
+    
+    if($conn->query($sql) == true) {
+        $Message = "Your Profile Updated Successfully";
+    }
+}
+
+if(isset($_POST['editusername']) && !empty($_POST['editusername'])) {
+
+    $editusername = $_POST['editusername'];
+    
+    $sql = "UPDATE admin SET username = '$editusername' WHERE id = '$Login_id'";
+    
+    if($conn->query($sql) == true) {
+        $Message = "Your Profile Updated Successfully";
+    }
+}
+
+echo $Message;
 ?> 
