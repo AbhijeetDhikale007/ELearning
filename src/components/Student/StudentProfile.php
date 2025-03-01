46d4fdfba14f3c69c30cc4a81e1bfe2513f32bff
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-03-01 14:09:11 +0530

    Code Simplified

--- src/components/Student/StudentProfile.php
+++ src/components/Student/StudentProfile.php
@@ -5,7 +5,6 @@
 $sql = "SELECT * FROM students where id='$Login_id'";
 $result = $conn->query($sql);
 
-
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         $id = $row['id'];
