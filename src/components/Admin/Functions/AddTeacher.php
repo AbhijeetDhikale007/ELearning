bcbf62aef8d478f2ab5bce55188008152ce96439
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-03-02 16:17:29 +0530

    Added: Function To Add Instructors

---
+++ src/components/Admin/Functions/AddTeacher.php
@@ -0,0 +1,22 @@
+<?php
+$DB_file = 'C:\xampp\htdocs\ELearning\src\components\dBconnection.php';
+
+require $DB_file;
+
+if(isset($_POST['newtname']) && isset($_POST['newprofileurl']) && isset($_POST['newemail']) && isset($_POST['newprofession']) && isset($_POST['newpassword'])) {
+
+    $newtname = $_POST['newtname'];
+    $newprofileurl = $_POST['newprofileurl'];
+    $newemail = $_POST['newemail'];
+    $newprofession = $_POST['newprofession'];
+    $newpassword = $_POST['newpassword'];
+
+    $sql = "INSERT INTO teachers (tname, profile_pic, email, profession, password) VALUES ('$newtname', '$newprofileurl', '$newemail', '$newprofession', '$newpassword')";
+    
+    if($conn->query($sql) == true) {
+        echo "New Instructor Added Successfully";
+    } else {
+        echo "Failed To Add New Instructor";
+    }
+}
+?>
