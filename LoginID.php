bb30bfa72b90c3b76efbad179cc9a3d71a8be3a0
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-03-03 14:46:55 +0530

    Session Start Code Added As A Comment

--- LoginID.php
+++ LoginID.php
@@ -1,4 +1,16 @@
 <?php
 // By default set to 1, change it with login.
 $Login_id = 1;
+
+// session_start();
+// include 'components/dBconnection.php';
+
+// if (isset($_SESSION['id'])) {
+//     $Login_id = $_SESSION['id'];
+// }
+
+// else {
+//     header('Location: ../authentication/login.html');
+//     exit;
+// }
 ?>
