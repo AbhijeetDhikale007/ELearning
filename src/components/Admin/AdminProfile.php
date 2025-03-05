8bde9da3f628a4d720fab17354e25f2eba2ce239
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2025-03-05 11:53:07 +0530

    Name Change

--- src/components/Admin/AdminProfile.php
+++ src/components/Admin/AdminProfile.php
@@ -28,7 +28,7 @@ if (!$result) {
             <img src=<?php echo $ImgLink; ?> alt="Profile">
             <div class='h-100 flex flex-col gap-20px'>
                 <p><?php echo $Name; ?></p>
-                <p class='text-[1.2vw]'>Instructor/Admin</p>
+                <p class='text-[1.2vw]'>Admin</p>
                 <p class='text-[1.1vw]'><?php echo $Email; ?></p>
                 <p class='text-[1vw]'><?php echo $Num; ?></p>
                 <div class='flex justify-end items-end'>
